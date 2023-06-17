3d82062abc9c39163e8998d5a959c4f4f45cff9f
Daniel S <dmschopler@gmail.com>
2023-06-17 15:50:09 -0400

    Added view staff and add staff functions

--- questions/questions.js
+++ questions/questions.js
@@ -1,13 +1,13 @@
 const inquirer = require('inquirer');
-const db = require('server');
+const db = require('./server');
 
 function start() {
     inquirer
       .prompt([
         {
             type: 'list',
-            name: 'beginningOptions',
             message: 'What would you like to do?',
+            name: 'beginningOptions',
             choices: [
                 'View all Employees',
                 'Add Employee',
@@ -46,5 +46,87 @@ function start() {
 }
 
 const viewStaff = () => {
+    db.query(
+        'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, AS Department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee e INNER JOIN roles r ON employee.role_id = role.id INNER JOIN departments d on role.departments_id = department.id LEFT JOIN employee ON employee.manager_id = manager.id',
+        (err, res) => {
+            if (err) throw err;
+            start();
+        }
+    );
+};
+
+const addStaff = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the first name of the new employee?',
+                name: 'first',
+            },
+            {
+                type: 'input',
+                message: 'What is the last name of the new employee?',
+                name: 'last',
+            },
+        ])
+        .then((answer) => {
+            const employeeFirst = answer.first;
+            const employeeLast = answer.last;
+            db.query('SELECT * FROM roles', (err, res) => {
+                if (err) throw err;
+                inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        message: 'What is the role of the new employee?',
+                        name: 'newRole',
+                        choices: res.map((role) => role.title),
+                    },
+                ])
+                .then((answer) => {
+                    const selectedRole = res.find(
+                        (role) => role.title === answer.newRole
+                    );
+                    db.query('SELECT * FROM employee', (err, res) => {
+                        if (err) throw err;
+                        inquirer
+                        .prompt([
+                            {
+                            type: 'list',
+                            message: 'Who is the new employee manager?',
+                            name: 'newEmployeeManager',
+                            choices: res.map(
+                                (employee) => employee.first_name + ' ' + employee.last_name
+                            ),
+                            },
+                        ])
+                        .then((answer) => {
+                            const selectedEmployee = res.find(
+                                (employee) => employee.first_name + ' ' + employee.last_name === answer.newEmployeeManager
+                            );
+                            db.query(
+                                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)',
+                                [
+                                    employeeFirst,
+                                    employeeLast,
+                                    selectedRole.id,
+                                    selectedEmployee.id,
+                                ],
+                                (err, res) => {
+                                    if (err) throw err;
+                                    else {
+                                        console.log('Employee added to database!');
+                                    }
+                                    start();
+                                }
+                            );
+                        });
+                    });
+                });
+            });
+        });
+};
+
+const updateStaff = () => {
     
 }
